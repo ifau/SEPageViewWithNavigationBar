aebcd76cedf376dc931ab87c14172bcbb1311c62
ifau <ifau@me.com>
2015-08-25 21:21:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,139 @@
+SEPageViewWithNavigationBar
+===========================
+
+SEPageViewWithNavigationBar is a subclass of UIViewController which contains UIPageViewController and changes navigation bar title during the swipe pages, like in Twitter app.
+
+![gif](https://github.com/ifau/SEPageViewWithNavigationBar/blob/master/Readme/1.gif?raw=true)
+
+#Usage
+
+Just create instance of SEPageViewWithNavigationBar and set view controllers to `viewControllers` property. Note that ***SEPageViewWithNavigationBar should be embedded in UINavigationController***.
+
+```swift
+let storyboard = UIStoryboard(name: "Main", bundle: nil)
+let vc1 = storyboard.instantiateViewControllerWithIdentifier("vc1") as! UIViewController
+let vc2 = storyboard.instantiateViewControllerWithIdentifier("vc2") as! UIViewController
+let vc3 = storyboard.instantiateViewControllerWithIdentifier("vc3") as! UIViewController
+
+let pagedViewController = SEPageViewWithNavigationBar()
+pagedViewController.viewControllers = [vc1, vc2, vc3]
+    
+self.window?.rootViewController = UINavigationController(rootViewController: pagedViewController)
+```
+
+#Configuration
+
+SEPageViewWithNavigationBar allows you to change the following properties:
+
+* `titleLabelFont : UIFont`
+	
+	Font of title label, __default__ is __UIFont.systemFontOfSize(16)__.
+	
+* `titleLabelTextColor : UIColor`
+	
+	Color of title label, __default__ is __UIColor.whiteColor()__.
+
+* `pageIndicatorTintColor : UIColor`
+	
+	Tint color of page indicator, __default__ is __UIColor(white: 1.0, alpha: 0.4)__.
+	
+* `currentPageIndicatorTintColor : UIColor`
+	
+	Tint color of current page indicator, __default__ is __UIColor.whiteColor()__.
+
+#Customization
+
+By default navigation bar title displays current view controller title. Actually, navigation bar title view in SEPageViewWithNavigationBar is UICollectionView, so you can define your own UICollectionViewCell and customize it as you need, using following function:
+
+`setCustomTitle(#cellClass: AnyClass, delegateCallback: ((titleCell: UICollectionViewCell, currentPage: Int) -> (UICollectionViewCell)))`
+
+For example, if you need image near title label:
+
+```swift
+class MyCustomCell: UICollectionViewCell
+{
+    var textLabel : UILabel!
+    var imageView : UIImageView!
+    
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+        
+        textLabel = UILabel()
+        textLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        textLabel.font = UIFont(name: "HelveticaNeue-Light", size: 20)
+        textLabel.textColor = UIColor.whiteColor()
+        self.contentView.addSubview(textLabel)
+        
+        imageView = UIImageView()
+        imageView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.contentView.addSubview(imageView)
+        
+        let space1 = UIView()
+        space1.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.contentView.addSubview(space1)
+        
+        let space2 = UIView()
+        space2.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.contentView.addSubview(space2)
+        
+        let views = ["textLabel" : textLabel, "imageView" : imageView, "space1" : space1, "space2" : space2]
+        
+        self.contentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[space1(>=0)][imageView]-4-[textLabel][space2(==space1)]|", options: .DirectionLeftToRight, metrics: nil, views: views))
+     
+        self.contentView.addConstraint(NSLayoutConstraint(item: imageView, attribute: .CenterY, relatedBy: .Equal, toItem: self.contentView, attribute: .CenterY, multiplier: 1, constant: 0))
+        self.contentView.addConstraint(NSLayoutConstraint(item: textLabel, attribute: .CenterY, relatedBy: .Equal, toItem: self.contentView, attribute: .CenterY, multiplier: 1, constant: 0))
+    }
+    
+    convenience init()
+    {
+        self.init(frame:CGRectZero)
+    }
+}
+```
+
+
+```swift
+self.window?.rootViewController = UINavigationController(rootViewController: pagedViewController)
+        
+pagedViewController.setCustomTitle(cellClass: MyCustomCell.self) { (titleCell: UICollectionViewCell, pageIndex: Int) -> (UICollectionViewCell) in
+                
+    let cell = titleCell as! MyCustomCell
+            
+    let titles = ["Explore", "Favorites", "Random"]
+    let images = [UIImage(named: "Explore"), UIImage(named: "Favorites"), UIImage(named: "Random")]
+            
+    cell.textLabel.text = titles[pageIndex]
+    cell.imageView.image = images[pageIndex]
+            
+    return cell
+}
+```
+
+Result:
+
+![custom title](https://github.com/ifau/SEPageViewWithNavigationBar/blob/master/Readme/2.png?raw=true)
+
+#License
+
+	The MIT License (MIT)
+
+	Copyright (c) 2015 Seliverstov Evgeney
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
---
+++ SEPageViewWithNavigationBar.swift
@@ -0,0 +1,313 @@
+//
+//  SEPageViewWithNavigationBar.swift
+//  PageViewWithNavigationBar
+//
+//  https://github.com/ifau/SEPageViewWithNavigationBar
+//
+//  Created by Seliverstov Evgeney on 15/08/15.
+//  Copyright (c) 2015 Seliverstov Evgeney. All rights reserved.
+//
+
+import UIKit
+
+class SEPageViewWithNavigationBar: UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate, UIScrollViewDelegate, UICollectionViewDataSource, UICollectionViewDelegate
+{
+    var pageIndicatorTintColor : UIColor = UIColor(white: 1.0, alpha: 0.4)
+    {
+        didSet
+        {
+            if titleView != nil
+            {
+                titleView.pageControl.pageIndicatorTintColor = pageIndicatorTintColor
+            }
+        }
+    }
+    
+    var currentPageIndicatorTintColor : UIColor = UIColor.whiteColor()
+    {
+        didSet
+        {
+            if titleView != nil
+            {
+                titleView.pageControl.currentPageIndicatorTintColor = currentPageIndicatorTintColor
+            }
+        }
+    }
+    
+    var titleLabelFont : UIFont = UIFont.systemFontOfSize(16)
+    {
+        didSet
+        {
+            if titleView != nil
+            {
+                titleView.collectionView.reloadItemsAtIndexPaths([NSIndexPath(forRow: currentPage, inSection: 0)])
+            }
+        }
+    }
+    
+    var titleLabelTextColor : UIColor = UIColor.whiteColor()
+    {
+        didSet
+        {
+            if titleView != nil
+            {
+                titleView.collectionView.reloadItemsAtIndexPaths([NSIndexPath(forRow: currentPage, inSection: 0)])
+            }
+        }
+    }
+    
+    var viewControllers : [UIViewController] = []
+    
+    private var currentPage : Int = 0
+    private var pageViewController : UIPageViewController!
+    private var titleView : SENavigationBarView!
+    
+    private var customTitleCell : ((titleCell: UICollectionViewCell, pageIndex: Int) -> (UICollectionViewCell))?
+    private var customTitleCellClass: AnyClass?
+    
+    // MARK: - Initialization
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        initialization()
+    }
+    
+    private func initialization()
+    {
+        if viewControllers.count > 0
+        {
+            pageViewController = UIPageViewController(transitionStyle: .Scroll, navigationOrientation: .Horizontal, options: nil)
+            pageViewController.dataSource = self
+            pageViewController.delegate = self
+            
+            pageViewController.setViewControllers([viewControllers[0]], direction: .Forward, animated: false, completion: nil)
+            
+            // self.addChildViewController(pageViewController)
+            self.view.addSubview(pageViewController.view)
+            // pageViewController.didMoveToParentViewController(self)
+            
+            for view in pageViewController.view.subviews
+            {
+                if let scrollView = view as? UIScrollView
+                {
+                    scrollView.delegate = self
+                }
+            }
+            
+            if self.navigationController != nil
+            {
+                let barframe = self.navigationController!.navigationBar.frame
+                let titleframe = CGRect(x: 0, y: 0, width: barframe.size.width - 88, height: barframe.size.height)
+                
+                titleView = SENavigationBarView(frame: titleframe)
+                titleView.pageControl.numberOfPages = viewControllers.count
+                titleView.pageControl.currentPage = 0
+                titleView.pageControl.pageIndicatorTintColor = pageIndicatorTintColor
+                titleView.pageControl.currentPageIndicatorTintColor = currentPageIndicatorTintColor
+                titleView.collectionView.delegate = self
+                titleView.collectionView.dataSource = self
+                self.navigationItem.titleView = titleView
+                
+                if customTitleCell != nil && customTitleCellClass != nil
+                {
+                    titleView.collectionView.registerClass(customTitleCellClass!, forCellWithReuseIdentifier: "customHeaderCell")
+                }
+            }
+        }
+    }
+    
+    // MARK: - UIPageViewController delegate
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController?
+    {
+        if let index = find(viewControllers, viewController)
+        {
+            return index + 1 == viewControllers.count ? nil : viewControllers[(index + 1)]
+        }
+        
+        return nil
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController?
+    {
+        if let index = find(viewControllers, viewController)
+        {
+            return index - 1 < 0 ? nil : viewControllers[(index - 1)]
+        }
+        
+        return nil
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [AnyObject], transitionCompleted completed: Bool)
+    {
+        let currentViewController = pageViewController.viewControllers.last as! UIViewController
+        
+        if let currentIndex = find(viewControllers, currentViewController)
+        {
+            currentPage = currentIndex
+            titleView.pageControl.currentPage = currentIndex
+        }
+    }
+    
+    // MARK: - UIScrollView delegate
+    
+    func scrollViewDidScroll(scrollView: UIScrollView)
+    {
+        if !(scrollView is UICollectionView) && titleView != nil
+        {
+            let allScrollViewContentSize = CGFloat(viewControllers.count) * self.view.frame.size.width
+            let currentScrollViewContentOffset = (CGFloat(currentPage) * self.view.frame.size.width) + (scrollView.contentOffset.x - self.view.frame.size.width)
+            let scrollPercent = currentScrollViewContentOffset * 100 / allScrollViewContentSize
+
+            titleView.collectionView.contentOffset = CGPoint(x: titleView.collectionView.contentSize.width * scrollPercent / 100, y: 0.0)
+        }
+    }
+    
+    // MARK: - UICollectionView delegate
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell
+    {
+        if customTitleCell != nil
+        {
+            let cell = collectionView.dequeueReusableCellWithReuseIdentifier("customHeaderCell", forIndexPath: indexPath) as! UICollectionViewCell
+            return customTitleCell!(titleCell: cell, pageIndex: indexPath.row)
+        }
+        else
+        {
+            let cell = collectionView.dequeueReusableCellWithReuseIdentifier("headerCell", forIndexPath: indexPath) as! SENavigationBarTitleCell
+            let viewController = viewControllers[indexPath.row]
+            
+            cell.label.text = viewController.title == nil ? "" : viewController.title
+            cell.label.font = titleLabelFont
+            cell.label.textColor = titleLabelTextColor
+            
+            return cell
+        }
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize
+    {
+        return collectionView.frame.size
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int
+    {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return viewControllers.count
+    }
+    
+    // MARK: - Other
+    
+    func setCustomTitle(#cellClass: AnyClass, delegateCallback: ((titleCell: UICollectionViewCell, currentPage: Int) -> (UICollectionViewCell)))
+    {
+        customTitleCellClass = cellClass
+        customTitleCell = delegateCallback
+    }
+    
+    override func willAnimateRotationToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation, duration: NSTimeInterval)
+    {
+        if let _titleView = self.navigationItem.titleView
+        {
+            let barframe = self.navigationController!.navigationBar.frame
+            let titleframe = CGRect(x: 0, y: 0, width: barframe.size.width - 88, height: barframe.size.height)
+            
+            _titleView.frame = titleframe
+        }
+    }
+}
+
+private class SENavigationBarView: UIView
+{
+    var collectionView : UICollectionView!
+    var pageControl : UIPageControl!
+    var fadeMask : CAGradientLayer!
+    
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+        initialization()
+    }
+
+    required init(coder aDecoder: NSCoder)
+    {
+        super.init(coder: aDecoder)
+        initialization()
+    }
+    
+    private func initialization()
+    {
+        var layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = UICollectionViewScrollDirection.Horizontal
+        layout.minimumInteritemSpacing = 0.0
+        layout.minimumLineSpacing = 0.0
+        
+        collectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: layout)
+        collectionView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        collectionView.registerClass(SENavigationBarTitleCell.self, forCellWithReuseIdentifier: "headerCell")
+        collectionView.backgroundColor = UIColor.clearColor()
+        collectionView.scrollEnabled = false
+        self.addSubview(collectionView)
+        
+        pageControl = UIPageControl()
+        pageControl.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.addSubview(pageControl)
+        
+        let views = ["collectionView" : collectionView, "pageControl": pageControl]
+        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-0-[collectionView]-0-|", options: NSLayoutFormatOptions.DirectionLeftToRight, metrics: nil, views: views))
+        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-0-[pageControl]-0-|", options: NSLayoutFormatOptions.DirectionLeftToRight, metrics: nil, views: views))
+        
+        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-0-[collectionView]-0-[pageControl(==8)]-2-|", options: NSLayoutFormatOptions.DirectionLeftToRight, metrics: nil, views: views))
+        
+        fadeMask = CAGradientLayer()
+        fadeMask.colors = [UIColor(white: 1.0, alpha: 0.0).CGColor, UIColor(white: 1.0, alpha: 1.0).CGColor, UIColor(white: 1.0, alpha: 1.0).CGColor, UIColor(white: 1.0, alpha: 0.0).CGColor];
+        fadeMask.locations = [0.0, 0.2, 0.8, 1.0]
+        fadeMask.startPoint = CGPoint(x: 0, y: 0)
+        fadeMask.endPoint = CGPoint(x: 1, y: 0)
+        self.layer.mask = fadeMask
+    }
+    
+    override func layoutSubviews()
+    {
+        let flowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        flowLayout.invalidateLayout()
+        
+        super.layoutSubviews()
+        
+        let offset = CGFloat(pageControl.currentPage) * collectionView.frame.size.width
+        collectionView.contentOffset = CGPoint(x: offset, y: 0.0)
+        fadeMask.frame = self.bounds
+    }
+}
+
+private class SENavigationBarTitleCell: UICollectionViewCell
+{
+    var label : UILabel!
+    
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+        
+        label = UILabel()
+        label.setTranslatesAutoresizingMaskIntoConstraints(false)
+        label.textAlignment = .Center
+        self.contentView.addSubview(label)
+        
+        self.contentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-0-[label]-0-|", options: NSLayoutFormatOptions.DirectionLeftToRight, metrics: nil, views: ["label" : label]))
+        self.contentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-0-[label]-0-|", options: NSLayoutFormatOptions.DirectionLeftToRight, metrics: nil, views: ["label" : label]))
+    }
+    
+    convenience init()
+    {
+        self.init(frame:CGRectZero)
+    }
+    
+    required init(coder aDecoder: NSCoder)
+    {
+        fatalError("This class does not support NSCoding")
+    }
+}
